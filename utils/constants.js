29ecf9e7545f57611e27fc9a26d8d0ad4bc97e98
Tolu Soneye <sammyjayden27@gmail.com>
2024-10-24 18:12:43 +0000

    backend url change

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL =
-  "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
+// export const BASE_URL =
+//   "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
+
+export const BASE_URL = "https://gaming-site-be.onrender.com/api/v1";
