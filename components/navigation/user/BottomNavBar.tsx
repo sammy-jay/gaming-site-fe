d5979f61671394ab69311621d6434c75a2d77d77
Tolu Soneye <sammyjayden27@gmail.com>
2024-11-02 10:59:13 +0000

    update

--- components/navigation/user/BottomNavBar.tsx
+++ components/navigation/user/BottomNavBar.tsx
@@ -1,5 +1,12 @@
-import Link from 'next/link';
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { IoIosArrowDown } from "react-icons/io";
 
 const BottomNavBar = () => {
   return (
@@ -11,82 +18,159 @@ const BottomNavBar = () => {
               <img src="https://www.cryptoplustrader.com/assets/images/logoIcon/logo.png" />
             </Link>
           </div>
-          <ul className="menu">
-            <li>
-              <Link href="/user/dashboard" className="active">
-                Dashboard
-              </Link>
-            </li>
-            <li>
-              <Link href="#" className="">
-                Practice
-              </Link>
-              <ul className="submenu">
-                <li>
-                  <Link href="/user/practice/trade">Practice Now</Link>
-                </li>
-                <li>
-                  <Link href="/user/practice/trade/log">Practice Log</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="#" className="">
-                Trade
-              </Link>
-              <ul className="submenu">
-                <li>
-                  <Link href="/user/trade">Trade Now</Link>
-                </li>
-                <li>
-                  <Link href="/user/trade/log">Trade Log</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="#" className="">
-                Deposit
-              </Link>
-              <ul className="submenu">
-                <li>
-                  <Link href="/user/deposit">Deposit Money</Link>
-                </li>
-                <li>
-                  <Link href="/user/deposit/history">Deposit History</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="#" className="">
-                Withdraw
-              </Link>
-              <ul className="submenu">
-                <li>
-                  <Link href="/user/withdraw">Withdraw Money</Link>
-                </li>
-                <li>
-                  <Link href="/user/withdraw/history">Withdraw History</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="#" className="">
-                Referral
-              </Link>
-              <ul className="submenu">
-                <li>
-                  <Link href="/user/referral/log">Referral Log</Link>
-                </li>
+          <div className="hidden lg:block">
+            <ul className="menu">
+              <li>
+                <Link href="/user/dashboard" className="active">
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger className="flex flex-row justify-start items-center">
+                      <Link href="#" className="">
+                        Practice
+                      </Link>
+                      <IoIosArrowDown className="w-5 h-5 -ml-2 text-white" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="flex flex-col gap-2 rounded-none m-0 p-0 bg-[#1E272E] border-0 outline-0  w-[100vw] lg:w-[200px]"
+                    >
+                      <Link
+                        href="/user/practice/trade"
+                        className="menu-nav-link"
+                      >
+                        Practice Now
+                      </Link>
+                      <div className="border-[0.5px] border-gray-700" />
+                      <Link
+                        href="/user/practice/trade/log"
+                        className="menu-nav-link"
+                      >
+                        Practice Log
+                      </Link>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
 
-                <li>
-                  <Link href="/user/commissions/history">Commission Log</Link>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <Link href="/user/transactions">Transaction Log</Link>
-            </li>
-          </ul>
+              <li>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger className="flex flex-row justify-start items-center">
+                      <Link href="#" className="">
+                        Trade
+                      </Link>
+                      <IoIosArrowDown className="w-5 h-5 -ml-2 text-white" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="flex flex-col gap-2 rounded-none m-0 p-0 bg-[#1E272E] border-0 outline-0 lg:w-[200px]"
+                    >
+                      <Link href="/user/trade" className="menu-nav-link">
+                        Trade Now
+                      </Link>
+                      <div className="border-[0.5px] border-gray-700" />
+                      <Link href="/user/trade/log" className="menu-nav-link">
+                        Trade Log
+                      </Link>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+
+              <li>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger className="flex flex-row justify-start items-center">
+                      <Link href="#" className="">
+                        Deposit
+                      </Link>
+                      <IoIosArrowDown className="w-5 h-5 -ml-2 text-white" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="flex flex-col gap-2 rounded-none m-0 p-0 bg-[#1E272E] border-0 outline-0 lg:w-[200px]"
+                    >
+                      <Link href="/user/deposit" className="menu-nav-link">
+                        Deposit Money
+                      </Link>
+                      <div className="border-[0.5px] border-gray-700" />
+                      <Link
+                        href="/user/deposit/history"
+                        className="menu-nav-link"
+                      >
+                        Deposit History
+                      </Link>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+
+              <li>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger className="flex flex-row justify-start items-center">
+                      <Link href="#" className="">
+                        Withdraw
+                      </Link>
+                      <IoIosArrowDown className="w-5 h-5 -ml-2 text-white" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="flex flex-col gap-2 rounded-none m-0 p-0 bg-[#1E272E] border-0 outline-0 lg:w-[200px]"
+                    >
+                      <Link href="/user/withdraw" className="menu-nav-link">
+                        Withdraw Money
+                      </Link>
+                      <div className="border-[0.5px] border-gray-700" />
+                      <Link
+                        href="/user/withdraw/history"
+                        className="menu-nav-link"
+                      >
+                        Withdraw History
+                      </Link>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+
+              <li>
+                <TooltipProvider delayDuration={200}>
+                  <Tooltip>
+                    <TooltipTrigger className="flex flex-row justify-start items-center">
+                      <Link href="#" className="">
+                        Referral
+                      </Link>
+                      <IoIosArrowDown className="w-5 h-5 -ml-2 text-white" />
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="flex flex-col gap-2 rounded-none m-0 p-0 bg-[#1E272E] border-0 outline-0 lg:w-[200px]"
+                    >
+                      <Link href="/user/referral/log" className="menu-nav-link">
+                        Referral Log
+                      </Link>
+                      <div className="border-[0.5px] border-gray-700" />
+                      <Link
+                        href="/user/commission/log"
+                        className="menu-nav-link"
+                      >
+                        Commission Log
+                      </Link>
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </li>
+
+              <li>
+                <Link href="/user/transactions">Transaction Log</Link>
+              </li>
+            </ul>
+          </div>
+
           <div className="header-bar m-0">
             <span></span>
             <span></span>
@@ -96,6 +180,6 @@ const BottomNavBar = () => {
       </div>
     </div>
   );
-}
+};
 
-export default BottomNavBar
+export default BottomNavBar;
