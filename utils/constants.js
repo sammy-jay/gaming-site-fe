59a7fa3730b58b54784ad5f1fa00c5b1d6524904
Tolu Soneye <sammyjayden27@gmail.com>
2024-11-09 03:35:08 +0000

    changed base url

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,5 @@
-export const BASE_URL =
-  "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
+// export const BASE_URL =
+//   "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
 
 // export const BASE_URL = "https://gaming-site-be.onrender.com/api/v1";
+export const BASE_URL = "https://gaming-site-be.vercel.app/api/v1";
