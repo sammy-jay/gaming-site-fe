810e3d1683fc93c44d2293209be7ad556b71f04f
Tolu Soneye <sammyjayden27@gmail.com>
2024-11-09 03:40:53 +0000

    merge

--- utils/constants.js
+++ utils/constants.js
@@ -1,9 +1,6 @@
 //export const BASE_URL =
  // "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
-// export const BASE_URL =
-//   "https://3001-gitpoddemos-votingapp-rfx3sfoea2t.ws-eu116.gitpod.io/api/v1";
 
 // export const BASE_URL = "https://gaming-site-be.onrender.com/api/v1";
 
-// export const BASE_URL = "https://gaming-site-be.onrender.com/api/v1";
 export const BASE_URL = "https://gaming-site-be.vercel.app/api/v1";
